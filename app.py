6b780216471283eec8a34bc4294002cc8112134c
Felg-Ts <el-feli-rubio1996@hotmail.com>
2021-05-08 17:11:52 +0200

    Errores
    Corrección de errores en las rutas de imágenes, hojas de estilos y plantillas

--- app.py
+++ app.py
@@ -1,53 +1,22 @@
-from flask import Flask, render_template
-import xml.etree.ElementTree as ET
-
+from flask import Flask, request,url_for,render_template
 app = Flask(__name__)	
 
-@app.route('/')
-def principal():
-    return render_template("principal.html")
+@app.route('/',methods=["GET","POST"])
+def inicio():
+    return render_template("inicio.html",titulo="principal")
 
-@app.route('/potencia/<int:bp>/<int:ep>/', methods=["GET","POST"])
-def potencia(bp, ep):
-    if ep <= -1:
-        ep2 = ep * -1
-        return render_template("potencia.html", base=bp, exponente=ep, solucion=bp**ep, solucion2=bp**ep2)
-    else:
-        return render_template("potencia.html", base=bp, exponente=ep, solucion=bp**ep)
+@app.route("/suma",methods=["GET","POST"])
+def sumar():
+	if request.method=="POST":
+		num1=request.form.get("num1")
+		num2=request.form.get("num2")
+		try:
+			resultado="Resultado de la suma:{}".format(str(int(num1)+int(num2)))
+		except:
+			resultado="No se puede realizar la suma"
+		return render_template("resultado.html",titulo="Resultado de la suma",resultado=resultado)
+	else:
+		return render_template("suma.html",titulo="Sumar")
 
-@app.route('/cuenta/<string:palabra>/<string:letra>/', methods=["GET","POST"])
-def cuentaletras(palabra, letra):
-    cont = 0
-
-    for i in palabra:
-        if i == letra:
-            cont = cont + 1
-    rels = (f"En la palabra {palabra} aparece {cont} veces el carácter {letra}")
-    return render_template("cuentaletras.html", aparece=rels)
-
-@app.route('/libro/<int:cod>/', methods=["GET","POST"])
-def libros(cod):
-
-    xmldoc = ET.parse('libros.xml')
-    root = xmldoc.getroot()
-
-    if cod == 123:
-        a1 = root.find("./libro[1]/autor")
-        t2 = root.find("./libro[1]/titulo")
-    elif cod == 124:
-        a1 = root.find("./libro[2]/autor")
-        t2 = root.find("./libro[2]/titulo")
-    elif cod == 125:
-        a1 = root.find("./libro[3]/autor")
-        t2 = root.find("./libro[3]/titulo")
-    elif cod == 126:
-        a1 = root.find("./libro[4]/autor")
-        t2 = root.find("./libro[4]/titulo")
-    elif cod == 127:
-        a1 = root.find("./libro[5]/autor")
-        t2 = root.find("./libro[5]/titulo")
-        
-    return render_template("libros.html", nombre=a1.text ,autor=t2.text)
-
-
-app.run("0.0.0.0",5000,debug=True)
+if __name__ == '__main__':
+	app.run('0.0.0.0',5000, debug=True)
